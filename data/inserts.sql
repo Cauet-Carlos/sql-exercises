072e935dcf53cee5a83f4a2cfb9f559ab7b9869e
Cauet-Carlos <cauetcarlos22@gmail.com>
2026-05-27 10:47:11 -0400

    Add SQL analytical challenges module

--- data/inserts.sql
+++ data/inserts.sql
@@ -18,6 +18,15 @@ INSERT INTO clientes (nome, email) VALUES
 ('João Pedro', 'joao@email.com'),
 ('Patricia Gomes', 'patricia@email.com');
 
+INSERT INTO clientes (nome, email) VALUES 
+('Renata Vasconcellos', 'renata.vasco@email.com'), 
+('Bruno Henrique', 'bruno.h@email.com'),           
+('Camila Pitanga', 'camila.p@email.com'),          
+('Diego Tardelli', 'diego.t@email.com'),           
+('Elena Gilbert', 'elena.g@email.com'),             
+('Felipe Melo', 'felipe.melo@email.com'),         
+('Gabriela Prioli', 'gabriela.p@email.com');     
+
 select * from clientes;
 
 -- =====================================================
@@ -34,13 +43,28 @@ INSERT INTO produtos (nome, preco) VALUES
 ('Webcam Full HD', 299.90),
 ('Mouse Vertical', 159.90);
 
+INSERT INTO produtos (nome, preco) VALUES
+('Mouse Pad Deskmat XXL', 89.90),
+('Microfone Condensador USB', 450.00),
+('Braço Articulado para Monitor', 220.00),
+('Roteador Wi-Fi 6 Mesh', 599.00),
+('SSD NVMe 1TB HighSpeed', 420.00),
+('Memória RAM DDR4 16GB', 310.00),
+('Placa de Vídeo RTX 4060', 2450.00),
+('Fonte Nominal 650W 80 Plus', 389.90),
+('Gabinete Mid Tower Vidro', 340.00),
+('Water Cooler 240mm RGB', 499.00),
+('Hub USB-C 7 em 1', 179.90),
+('Suporte Notebook Alumínio', 120.00),
+('Fita LED Smart Wi-Fi', 79.90);
+
 select * from produtos;
 
 -- =====================================================
 -- TABELA: PEDIDOS
 -- =====================================================
 INSERT INTO pedidos (id_cliente, data_pedido, status) VALUES 
--- Outubro
+-- Outubro 2023
 (1, '2023-10-01', 'Confirmado'),
 (2, '2023-10-02', 'Confirmado'),
 (1, '2023-10-03', 'Cancelado'),
@@ -51,21 +75,70 @@ INSERT INTO pedidos (id_cliente, data_pedido, status) VALUES
 (1, '2023-10-15', 'Confirmado');
 
 INSERT INTO pedidos (id_cliente, data_pedido, status) VALUES
--- Novembro
+-- Novembro 2023
 (2, '2023-11-03', 'Confirmado'),
 (3, '2023-11-08', 'Confirmado'),
 (5, '2023-11-15', 'Cancelado'),
 (1, '2023-11-20', 'Confirmado'),
--- Dezembro
+-- Dezembro 2023
 (4, '2023-12-02', 'Confirmado'),
 (2, '2023-12-10', 'Bloqueado'),
 (3, '2023-12-18', 'Confirmado'),
 (5, '2023-12-22', 'Confirmado'),
--- Janeiro
+-- Janeiro 2024
 (1, '2024-01-05', 'Confirmado'),
 (4, '2024-01-11', 'Cancelado'),
 (2, '2024-01-19', 'Confirmado'),
 (3, '2024-01-25', 'Confirmado');
+
+INSERT INTO pedidos (id_cliente, data_pedido, status) VALUES 
+-- Fevereiro e Março 2024
+(6, '2024-02-02', 'Confirmado'),  
+(7, '2024-02-10', 'Confirmado'),  
+(1, '2024-02-15', 'Confirmado'),  
+(9, '2024-02-22', 'Cancelado'),  
+(10, '2024-03-01', 'Confirmado'), 
+(2, '2024-03-12', 'Confirmado'),  
+(11, '2024-03-15', 'Bloqueado'),  
+(12, '2024-03-28', 'Confirmado'),
+
+-- Abril, Maio e Junho 
+(3, '2024-04-05', 'Confirmado'),  
+(13, '2024-04-18', 'Confirmado'), 
+(14, '2024-04-20', 'Cancelado'),  
+(9, '2024-05-02', 'Confirmado'),   
+(6, '2024-05-15', 'Confirmado'),  
+(15, '2024-05-25', 'Confirmado'), 
+(1, '2024-06-03', 'Confirmado'),  
+(4, '2024-06-12', 'Confirmado'),  
+(12, '2024-06-22', 'Cancelado'),  
+
+-- Julho, Agosto e Setembro 
+(7, '2024-07-01', 'Confirmado'),  
+(9, '2024-07-14', 'Confirmado'), 
+(2, '2024-08-05', 'Confirmado'),  
+(10, '2024-08-19', 'Confirmado'), 
+(13, '2024-08-25', 'Bloqueado'), 
+(15, '2024-09-02', 'Confirmado'), 
+(5, '2024-09-18', 'Confirmado'), 
+(11, '2024-09-30', 'Confirmado'), 
+
+-- Outubro, Novembro e Dezembro 2024
+(1, '2024-10-05', 'Confirmado'),  
+(6, '2024-10-20', 'Confirmado'),  
+(9, '2024-11-01', 'Confirmado'),  
+(14, '2024-11-10', 'Cancelado'),  
+(2, '2024-11-25', 'Confirmado'),  
+(10, '2024-11-26', 'Confirmado'), 
+(15, '2024-11-27', 'Confirmado'), 
+(3, '2024-11-28', 'Confirmado'), 
+(7, '2024-12-05', 'Confirmado'),  
+(9, '2024-12-12', 'Confirmado'),  
+(12, '2024-12-15', 'Confirmado'), 
+(13, '2024-12-18', 'Confirmado'), 
+(1, '2024-12-20', 'Confirmado'), 
+(4, '2024-12-22', 'Cancelado'),   
+(11, '2024-12-23', 'Confirmado'); 
 
 select * from pedidos;
 
@@ -73,59 +146,117 @@ select * from pedidos;
 -- TABELA: ITENS_PEDIDO
 -- =====================================================
 INSERT INTO itens_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES 
--- Pedido 1: Compras variadas
 (1, 1, 2, 189.90), 
 (1, 2, 1, 350.00), 
 (1, 3, 1, 1250.00),
--- Pedido 2: Cliente comprou monitor e teclado
 (2, 3, 1, 1250.00),
 (2, 2, 3, 350.00),
--- Pedido 3: Apenas um fone
 (3, 1, 1, 189.90),
--- Pedido 4: Itens de escritório
 (4, 4, 1, 890.00), 
 (4, 5, 2, 1950.00),
--- Pedido 5: Repetindo produtos em pedidos diferentes
 (5, 2, 2, 350.00),
 (5, 5, 2, 1950.00),
--- Pedido 6:
 (6, 3, 1, 1250.00), 
 (6, 1, 3, 189.90),
--- Pedido 7:
 (7, 4, 1, 890.00),
--- Pedido 8: O pedido mais completo
 (8, 5, 1, 1950.00), 
 (8, 1, 4, 189.90);
 
 INSERT INTO itens_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES
--- Pedido 9
 (9, 1, 1, 189.90),
 (9, 5, 1, 1950.00),
--- Pedido 10
 (10, 3, 2, 1250.00),
--- Pedido 11
 (11, 2, 1, 350.00),
--- Pedido 12
 (12, 4, 1, 890.00),
 (12, 1, 2, 189.90),
--- Pedido 13
 (13, 5, 1, 1950.00),
--- Pedido 14
 (14, 2, 2, 350.00),
--- Pedido 15
 (15, 3, 1, 1250.00),
 (15, 4, 1, 890.00),
--- Pedido 16
 (16, 1, 5, 189.90),
--- Pedido 17
 (17, 5, 2, 1950.00),
--- Pedido 18
 (18, 4, 1, 890.00),
--- Pedido 19
 (19, 2, 3, 350.00),
 (19, 3, 1, 1250.00),
--- Pedido 20
 (20, 1, 2, 189.90),
 (20, 5, 1, 1950.00);
 
+INSERT INTO itens_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES
+-- Pedidos de Fevereiro / Março 
+(21, 6, 1, 299.90), 
+(21, 8, 1, 89.90),
+(22, 12, 2, 420.00), 
+(22, 13, 4, 310.00),
+(23, 14, 1, 2450.00),
+(24, 7, 2, 159.90),
+(25, 5, 1, 1950.00), 
+(25, 9, 1, 450.00),
+(26, 10, 1, 220.00), 
+(26, 1, 1, 189.90),
+(27, 4, 1, 890.00),
+(28, 11, 1, 599.00), 
+(28, 18, 1, 120.00),
+
+-- Pedidos de Média Temporada (Abril a Junho)
+(29, 2, 1, 350.00), 
+(29, 19, 2, 79.90),
+(30, 15, 1, 389.90), 
+(30, 16, 1, 340.00),
+(31, 3, 1, 1250.00),
+(32, 14, 1, 2450.00), 
+(32, 12, 2, 420.00), 
+(32, 13, 2, 310.00),
+(33, 17, 1, 499.00),
+(34, 1, 1, 189.90), 
+(34, 7, 1, 159.90),
+(35, 4, 2, 890.00),
+(36, 5, 1, 1950.00),
+(37, 6, 1, 299.90),
+
+-- Pedidos do Meio do Ano
+(38, 14, 1, 2450.00), 
+(38, 15, 1, 389.90),
+(39, 2, 2, 350.00), 
+(39, 8, 3, 89.90),
+(40, 3, 1, 1250.00),
+(41, 14, 2, 2450.00),
+(42, 5, 1, 1950.00),
+(43, 12, 2, 420.00),
+(44, 13, 2, 310.00), 
+(44, 1, 1, 189.90),
+(45, 9, 1, 450.00), 
+(45, 10, 1, 220.00),
+
+-- Pedidos de Alta Sazonalidade 
+(46, 6, 2, 299.90), 
+(46, 18, 2, 120.00),
+(47, 3, 1, 1250.00), 
+(47, 2, 1, 350.00),
+(48, 14, 1, 2450.00), 
+(48, 12, 4, 420.00),
+(49, 5, 2, 1950.00),
+(50, 4, 1, 890.00), 
+(50, 14, 1, 2450.00), 
+(50, 1, 2, 189.90),
+(51, 3, 2, 1250.00), 
+(51, 17, 1, 499.00),
+(52, 5, 1, 1950.00), 
+(52, 6, 1, 299.90),
+(53, 11, 2, 599.00), 
+(53, 12, 2, 420.00),
+(54, 14, 1, 2450.00), 
+(54, 13, 2, 310.00),
+(55, 15, 1, 389.90), 
+(55, 16, 1, 340.00), 
+(55, 17, 1, 499.00),
+(56, 1, 1, 189.90), 
+(56, 2, 1, 350.00), 
+(56, 8, 1, 89.90),
+(57, 4, 1, 890.00),
+(58, 5, 1, 1950.00), 
+(58, 14, 1, 2450.00), 
+(59, 3, 1, 1250.00),
+(60, 9, 2, 450.00), 
+(60, 10, 2, 220.00), 
+(60, 19, 3, 79.90);
 select * from itens_pedido;
---
+++ sql/07-analytical_challenges.sql
@@ -0,0 +1,287 @@
+-- =================================================================
+-- ANALYTICAL CHALLENGES
+-- =================================================================
+
+-- ========================================================================
+-- Clientes_vip
+-- ========================================================================
+WITH faturamento_clientes as (
+	SELECT
+		c.id_cliente as id,
+		c.nome as cliente,
+		ROUND(SUM(ip.quantidade * ip.preco_unitario), 2) as faturamento
+	FROM clientes as c
+		INNER JOIN pedidos as ped
+			ON c.id_cliente = ped.id_cliente
+		INNER JOIN itens_pedido as ip
+			ON ped.id_pedido = ip.id_pedido
+	WHERE ped.status = 'Confirmado'
+	GROUP BY c.id_cliente, c.nome
+),
+classificacao_clientes as (
+	SELECT
+		id, cliente, faturamento,
+        NTILE(4)
+			OVER(order by faturamento)
+		as classificacao_cliente
+    FROM faturamento_clientes
+)
+SELECT
+	id, cliente, faturamento,
+    CASE
+		WHEN classificacao_cliente = 4 then 'VIP'
+        WHEN classificacao_cliente IN (2, 3) then 'ALTO'
+        ELSE 'BAIXO'
+	END as ranking
+FROM classificacao_clientes
+WHERE classificacao_cliente = 4
+ORDER BY faturamento DESC;
+
+-- ========================================================================
+-- Produtos mais rentaveis
+-- ========================================================================
+WITH faturamento_produtos as (
+	SELECT
+		prod.id_produto as id,
+		prod.nome as produto,
+		ROUND(SUM(ip.quantidade * ip.preco_unitario), 2) as faturamento
+	FROM produtos as prod
+			INNER JOIN itens_pedido as ip
+				ON prod.id_produto = ip.id_produto
+			INNER JOIN pedidos as ped
+				ON ped.id_pedido = ip.id_pedido
+	WHERE ped.status = 'Confirmado'
+	GROUP BY prod.id_produto, prod.nome
+)
+SELECT
+	id, produto, faturamento,
+    DENSE_RANK() 
+		OVER(order by faturamento DESC) 
+	as ranking
+FROM faturamento_produtos;
+
+-- ========================================================================
+-- Analise cancelamentos
+-- ========================================================================
+WITH total_cancelamentos_cliente as (
+	SELECT
+		c.id_cliente as id,
+		c.nome as cliente,
+		COUNT(ped.id_pedido) as qtd_ped_cancelados
+	FROM pedidos as ped
+		INNER JOIN clientes as c
+			ON c.id_cliente = ped.id_cliente
+	WHERE ped.status = 'Cancelado'
+	GROUP BY c.id_cliente, c.nome
+)
+SELECT
+	id, cliente, qtd_ped_cancelados,
+	SUM(qtd_ped_cancelados) 
+		OVER() 
+	as total_pedidos_cancelados,
+	ROUND((qtd_ped_cancelados * 100.0) / SUM(qtd_ped_cancelados) 
+		OVER(), 2) 
+	as percentual_do_total
+FROM total_cancelamentos_cliente
+ORDER BY qtd_ped_cancelados DESC;
+
+-- ========================================================================
+-- Crescimento mensal
+-- ========================================================================
+WITH faturamento_mensal as (
+	SELECT
+		YEAR(ped.data_pedido) as ano,
+		MONTH(ped.data_pedido) as mes,
+		ROUND(SUM(ip.preco_unitario * ip.quantidade), 2) as faturamento
+	FROM pedidos as ped
+		INNER JOIN itens_pedido as ip
+			ON ped.id_pedido = ip.id_pedido
+	WHERE ped.status = 'Confirmado'
+	GROUP BY YEAR(ped.data_pedido), MONTH(ped.data_pedido)
+),
+analise_faturamento as (
+	SELECT
+		ano, mes, faturamento,
+        LAG(faturamento) 
+			OVER(order by ano, mes)
+		as faturamento_anterior
+    FROM faturamento_mensal
+)
+SELECT
+	ano, mes, faturamento,
+    COALESCE (
+		ROUND(
+			((faturamento - faturamento_anterior) * 100.0) /
+			NULLIF(faturamento_anterior, 0),
+		2)
+	, 0) as crescimento_mensal
+FROM analise_faturamento;
+
+-- ========================================================================
+-- Segmentação clientes
+-- ========================================================================
+WITH faturamento_clientes as (
+	SELECT
+		c.id_cliente as id,
+		c.nome as cliente,
+		ROUND(SUM(ip.quantidade * ip.preco_unitario), 2) as faturamento
+	FROM clientes as c
+		INNER JOIN pedidos as ped
+			ON c.id_cliente = ped.id_cliente
+		INNER JOIN itens_pedido as ip
+			ON ped.id_pedido = ip.id_pedido
+	WHERE ped.status = 'Confirmado'
+	GROUP BY c.id_cliente, c.nome
+)
+SELECT
+	id, cliente, faturamento,
+    CASE
+		WHEN faturamento >= 10000 then 'VIP'
+        WHEN faturamento >= 5000 then 'OURO'
+        WHEN faturamento BETWEEN 2000 AND 4900 THEN 'PRATA'
+        ELSE 'BRONZE'
+	END as ranking
+FROM faturamento_clientes
+ORDER BY faturamento DESC;
+
+-- ========================================================================
+-- Ticket médio clientes
+-- ========================================================================
+WITH ticket_medio_clientes AS (
+    SELECT
+        c.id_cliente,
+        c.nome,
+        ROUND(
+            SUM(ip.quantidade * ip.preco_unitario) /
+            COUNT(DISTINCT ped.id_pedido),
+        2) AS ticket_medio
+    FROM clientes c
+		INNER JOIN pedidos ped
+			ON c.id_cliente = ped.id_cliente
+		INNER JOIN itens_pedido ip
+			ON ped.id_pedido = ip.id_pedido
+    WHERE ped.status = 'Confirmado'
+    GROUP BY c.id_cliente, c.nome
+)
+SELECT *
+FROM ticket_medio_clientes
+ORDER BY ticket_medio DESC;
+
+-- ========================================================================
+-- Top produtos por mês
+-- ========================================================================
+WITH faturamento_produtos_mes as (
+	SELECT
+		YEAR(ped.data_pedido) as ano,
+		MONTH(ped.data_pedido) as mes,
+        prod.nome as produto,
+		ROUND(SUM(ip.preco_unitario * ip.quantidade), 2) as faturamento
+	FROM pedidos as ped
+		INNER JOIN itens_pedido as ip
+			ON ped.id_pedido = ip.id_pedido
+		INNER JOIN produtos as prod
+			ON prod.id_produto = ip.id_produto
+	WHERE ped.status = 'Confirmado'
+	GROUP BY YEAR(ped.data_pedido), MONTH(ped.data_pedido), prod.nome
+),
+ranking_produtos as (
+	SELECT
+		ano, mes, produto, faturamento,
+		DENSE_RANK()
+			OVER(
+				partition by ano, mes
+				order by faturamento DESC
+			) 
+		as ranking_vendas_produtos
+	FROM faturamento_produtos_mes
+)
+SELECT
+	ano, 
+	mes, 
+	ranking_vendas_produtos as posicao,
+	produto, 
+	faturamento
+FROM ranking_produtos
+WHERE ranking_vendas_produtos <= 3 
+ORDER BY ano DESC, mes DESC, posicao ASC;
+
+-- ========================================================================
+-- Análise de Retenção Geral (Fidelidade do Cliente)
+-- ========================================================================
+WITH historico_compras_cliente AS (
+	SELECT
+		id_cliente,
+		COUNT(id_pedido) AS total_compras
+	FROM pedidos
+	WHERE status = 'Confirmado'
+	GROUP BY id_cliente
+),
+faixas_retencao AS (
+	SELECT
+		id_cliente,
+		total_compras,
+		CASE 
+			WHEN total_compras = 1 THEN '1. Comprou apenas 1 vez (Sem Retenção)'
+			WHEN total_compras = 2 THEN '2. Comprou 2 vezes (Início de Retenção)'
+			WHEN total_compras BETWEEN 3 AND 5 THEN '3. Recorrente (3 a 5 compras)'
+			ELSE '4. Altamente Fiel (Mais de 5 compras)'
+		END AS status_retencao
+	FROM historico_compras_cliente
+)
+SELECT
+	status_retencao,
+	COUNT(id_cliente) AS qtd_clientes,
+	ROUND(
+		(COUNT(id_cliente) * 100.0) / SUM(COUNT(id_cliente)) OVER(), 
+		2
+	) AS percentual_da_base
+FROM faixas_retencao
+GROUP BY status_retencao
+ORDER BY status_retencao ASC;
+
+-- ========================================================================
+-- Faturamento acumulado
+-- ========================================================================
+WITH faturamento_mensal as (
+	SELECT
+		YEAR(ped.data_pedido) as ano,
+		MONTH(ped.data_pedido) as mes,
+		ROUND(SUM(ip.preco_unitario * ip.quantidade), 2) as faturamento
+	FROM pedidos as ped
+		INNER JOIN itens_pedido as ip
+			ON ped.id_pedido = ip.id_pedido
+	WHERE ped.status = 'Confirmado'
+	GROUP BY YEAR(ped.data_pedido), MONTH(ped.data_pedido)
+)
+SELECT
+	ano, mes, faturamento,
+    SUM(faturamento)
+		OVER(
+			partition by ano
+			order by ano, mes
+		)
+	as faturamento_acumulado
+FROM faturamento_mensal;
+
+-- ========================================================================
+-- Ranking clientes
+-- ========================================================================
+WITH faturamento_clientes as (
+	SELECT
+		c.id_cliente as id,
+		c.nome as cliente,
+		ROUND(SUM(ip.quantidade * ip.preco_unitario), 2) as faturamento
+	FROM clientes as c
+		INNER JOIN pedidos as ped
+			ON c.id_cliente = ped.id_cliente
+		INNER JOIN itens_pedido as ip
+			ON ped.id_pedido = ip.id_pedido
+	WHERE ped.status = 'Confirmado'
+	GROUP BY c.id_cliente, c.nome
+)
+SELECT
+	id, cliente, faturamento,
+    DENSE_RANK()
+		OVER(order by faturamento DESC)
+	as ranking
+FROM faturamento_clientes;
